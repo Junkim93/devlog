208912771794b25afbf15941b1afff3afc1c0513
Jun Kim <93kimhyunjun@gmail.com>
2019-06-04 00:13:11 +0900

    edit some styles

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://junkim.netlify.com',
-  title: 'devlog.jun👨🏻‍💻',
-  subtitle: '주 7일 공부 잊지마',
-  copyright: '© All rights reserved.',
-  disqusShortname: 'https-junkim-netlify-com',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-140646384-1',
+  url: "https://junkim.netlify.com",
+  title: "devlog.jun👨🏻‍💻",
+  subtitle: "주 7일 공부 잊지마",
+  copyright: "© All rights reserved.",
+  disqusShortname: "https-junkim-netlify-com",
+  postsPerPage: 10,
+  googleAnalyticsId: "UA-140646384-1",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'Categories',
-      path: '/categories'
+      label: "Categories",
+      path: "/categories"
     },
     {
-      label: 'Tags',
-      path: 'tags'
+      label: "Tags",
+      path: "tags"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
-    },
+      label: "About me",
+      path: "/pages/about"
+    }
   ],
   author: {
-    name: '면준이',
-    photo: '/photo.jpg',
-    bio: '주 7일 공부 잊지마',
+    name: "면준이",
+    photo: "/photo.jpg",
+    bio: "주 7일 공부 잊지마",
     contacts: {
-      email: '93kimhyunjun@gmail.com',
-      github: 'junkim93'
+      email: "93kimhyunjun@gmail.com",
+      github: "junkim93"
     }
   }
 };
