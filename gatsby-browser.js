49b6a3c78a90b3dc970150416e781934a2e6cbb5
Jun Kim <93kimhyunjun@gmail.com>
2019-11-14 23:50:28 +0900

    Fix: prism css error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-require("./src/assets/scss/init.scss");
-require("prismjs/themes/prism-okaidia.css");
+require('./src/assets/scss/init.scss');
+require('prismjs/themes/prism-tomorrownight.css');
 
 exports.onClientEntry = () => {};
